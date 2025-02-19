e86641587a7010da8f3a6f69d661ac042408eb46
Lavinia888 <159921187+Lavinia888@users.noreply.github.com>
2025-02-19 15:23:29 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+AOS.init({
+    duration: 800,
+    once: true
+});
+
+new SimpleLightbox({elements: ".activitate-galerie a"});
+
+
+function mobileMenu() {
+    var x = document.getElementsByTagName("nav")[0];
+    if (x.className === "") {
+        x.className += "mobile";
+    } else {
+        x.className = "";
+    }
+
+}
+
